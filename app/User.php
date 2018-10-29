b18c8be85456497ff45617df22c4c697e5a98410
Ahmad Djunaedi <ahmaddjunaedi92@gmail.com>
2018-10-29 10:25:51 +0700

    change style field db

--- app/User.php
+++ app/User.php
@@ -10,13 +10,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $primaryKey = 'UserId';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','provider','provider_id'
+        'FullName', 'Email', 'Password','PhoneNumber','Provider','ProviderId'
     ];
 
     /**
@@ -25,6 +27,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'Password', 'remember_token',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
     ];
 }
